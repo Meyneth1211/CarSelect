790b01fce6fabbad4cbc2929a59cc170262dccc1
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-02 15:08:56 +0900

    login.cssファイル作成内容はまだ。検索フォルダ会員フォルダに移動
    login.cssファイル作成内容はまだ。検索フォルダ会員フォルダに移動。

---
+++ kaiin/login.php
@@ -0,0 +1,19 @@
+<?php require('../header/header.html');?>
+    <div class="login-1">
+        <h1>ログイン</h1>
+        <div class="login-form">
+            <form class="login-form-in" action="" method="post">
+                <p>メールアドレス</p>
+                <input class="text" type="email" name="mail" placeholder="  Email">
+                <p>パスワード</p>
+                <input class="text" type="password" name="password" placeholder="  Password"><br><br>
+
+                <input class="button" type="submit" value="ログイン"><br><br>
+            </form>
+            <form class="login-form-new" action="" method="post">
+                <input class="button" type="submit" value="アカウント新規作成">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
