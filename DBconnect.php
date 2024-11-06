a5d3994b5cd17496cd750f4c1960c0b0a12257da
Meyneth1211 <2301389@s.asojuku.ac.jp>
2024-11-06 11:55:24 +0900

    DB接続用コードをメソッド化
    データベース接続を行うコードを他のプログラムから再利用できるようメソッド化

--- DBconnect.php
+++ DBconnect.php
@@ -1,4 +1,13 @@
 <?php
+    function getDB(){
+        $dsn='mysql:host=mysql311.phy.lolipop.lan;dbname=LAA1557132-carselect;charset=utf8';
+        $username='LAA1557132';
+        $password='asovehiclesd2b';
+        $pdo=new PDO($dsn,$username,$password);
+
+        return $pdo;
+    }
+    
     $pdo=new PDO('mysql:host=mysql311.phy.lolipop.lan;
     dbname=LAA1557132-carselect;charset=utf8',
     'LAA1557132',
