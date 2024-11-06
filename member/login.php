ed8a4376f8eb953c5798f0d8a1efcbb3e6e2cbfd
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-06 09:52:11 +0900

    ログイン画面、会員登録画面作成
    login画面login.next画面作成

--- member/login.php
+++ member/login.php
@@ -1,17 +1,16 @@
-<?php require('../header/header.html');?>
-    <div class="login-1">
-        <h1>ログイン</h1>
-        <div class="login-form">
-            <form class="login-form-in" action="" method="post">
-                <p>メールアドレス</p>
-                <input class="text" type="email" name="mail" placeholder="  Email">
-                <p>パスワード</p>
-                <input class="text" type="password" name="password" placeholder="  Password"><br><br>
-
-                <input class="button" type="submit" value="ログイン"><br><br>
+<?php require('../header/header.php'); ?>
+ 
+    <div class="login">
+        <div class="login-card">
+            <h2>ログイン</h2>
+            <form class="login-form" action="">
+                <input class="mail" type="text" placeholder="Email">
+                <input class="pass" type="password" placeholder="Password">
+                <div class="top"></div>
+                <input class="button-1" type="submit" value="ログイン">
             </form>
-            <form class="login-form-new" action="" method="post">
-                <input class="button" type="submit" value="アカウント新規作成">
+            <form class="login-form" action="login-next.php" method="post">
+                    <input class="button-2" type="submit" value="アカウント新規作成">
             </form>
         </div>
     </div>
