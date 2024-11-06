a8907c0c6b9f70c9b826f558a6e48109082f55ab
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-06 21:32:55 +0900

    ちょっと修正

--- member/top.php
+++ member/top.php
@@ -4,9 +4,9 @@
     $pdo = getDb();
 
     //メールアドレスとパスワードが入力されているか確認
-    if(!empty($_POST['mail'])&&!empty($_POST['password'])){
+    if(!empty($_POST['mail'])&&!empty($_POST['pass'])){
         $sql=$pdo->prepare('SELECT * from user where user_mail=? and user_password=?');
-        $sql->execute([$_POST['mail'],$_POST['password']]);
+        $sql->execute([$_POST['mail'],$_POST['pass']]);
         foreach($sql as $row){
             $_SESSION['mail'] = $row['user_mail'];
             $_SESSION['name'] = $row['user_name'];
