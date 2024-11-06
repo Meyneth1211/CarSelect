db69f7a8247afdd12ef7a65038f88eb9f9cfb386
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-06 21:43:50 +0900

    login,php内にエラー処理記入
    login,php内にエラー処理記入

--- member/top.php
+++ member/top.php
@@ -1,27 +1,3 @@
-<?php
-    session_start();
-    require_once '../DBconnect.php';
-    $pdo = getDb();
-
-    //メールアドレスとパスワードが入力されているか確認
-    if(!empty($_POST['mail'])&&!empty($_POST['pass'])){
-        $sql=$pdo->prepare('SELECT * from user where user_mail=? and user_password=?');
-        $sql->execute([$_POST['mail'],$_POST['pass']]);
-        foreach($sql as $row){
-            $_SESSION['mail'] = $row['user_mail'];
-            $_SESSION['name'] = $row['user_name'];
-        }
-        if(!empty($_SESSION['name'])){
-            
-        }else{
-            echo '<div class="Error-Message">EmailかPasswordが違います</div><br>';
-            echo '<div class="Error-Message"><a href="login.php">戻る</a></div>';
-            exit;
-        }
-    }
-?>
-
-
 <?php require('../header/header.php'); ?>
     <link rel="stylesheet" href="../css/top.css">
     <div class="brand">
