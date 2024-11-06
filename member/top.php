b74c08336f0f67f4c8ea68acd410db71bd9baea6
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-06 12:49:28 +0900

    topページにログイン後入力確認処理作成
    topページにログイン後入力確認処理作成

--- member/top.php
+++ member/top.php
@@ -1,6 +1,29 @@
+<?php
+    session_start();
+    require_once 'DbManager.php';
+    $pdo = getDb();
+
+    //メールアドレスとパスワードが入力されているか確認
+    if(!empty($_POST['mail'])&&!empty($_POST['password'])){
+        $sql=$pdo->prepare('SELECT * from user where user_mail=? and user_password=?');
+        $sql->execute([$_POST['mail'],$_POST['password']]);
+        foreach($sql as $row){
+            $_SESSION['mail'] = $row['user_mail'];
+            $_SESSION['name'] = $row['user_name'];
+        }
+        if(!empty($_SESSION['name'])){
+            
+        }else{
+            echo 'EmailかPasswordが違います<br>';
+            echo '<a href="login.php">戻る</a>';
+            exit;
+        }
+    }
+?>
+
+
 <?php require('../header/header.php'); ?>
     <link rel="stylesheet" href="../css/top.css">
-<body>
     <div class="brand">
         <img src="" alt="">
     </div>
