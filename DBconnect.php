7f185e10539f3fe947da2b6a819040f6d456fcc4
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-06 21:57:44 +0900

    Update DBconnect.php

--- DBconnect.php
+++ DBconnect.php
@@ -8,18 +8,5 @@
         return $pdo;
     }
     
-    $pdo=new PDO('mysql:host=mysql311.phy.lolipop.lan;
-    dbname=LAA1557132-carselect;charset=utf8',
-    'LAA1557132',
-    'asovehiclesd2b');
-    foreach ($pdo->query('select * from user') as $row) {
-        # code...
-        echo '<p>';
-        echo $row['user_id'],':';
-        echo $row['user_name'],':';
-        echo $row['user_mail'],':';
-        echo $row['user_password'],':';
-        echo $row['user_address'],':';
-        echo '</p>';
-    }
+
 ?>
