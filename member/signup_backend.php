4f38d27864d278f6311c71c537a2c9e8c45877df
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-07 10:25:04 +0900

    foreach追加

--- member/signup_backend.php
+++ member/signup_backend.php
@@ -3,6 +3,7 @@
     require('../header/header.php');
     require_once '../DBconnect.php';
     $pdo = getDb();
+    //名前、メアド、パスワード、住所入力しているか確認の処理
     $error = []; //エラー配列作成
     if(empty($_POST['name'])){
         $error[] = '名前を入力してね！';
@@ -15,5 +16,12 @@
     }
     if(empty($_POST['address'])){
         $error[] = '住所を入力してね！';
+    }
+    foreach($error as $e){
+        echo '<p>'.$e.'</p>';
+    }
+    if(!empty($error)){
+        echo '<a href="signup.php">会員登録画面へ戻る</a>';
+        exit;
     }
 ?>
