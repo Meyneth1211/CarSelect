2f4cdde986cdf81a4ebac3624172d43696ea377c
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-08 18:55:19 +0900

    ｈ１タグテスト

--- member/top.php
+++ member/top.php
@@ -40,6 +40,7 @@
     }
 ?>
 <div id="slider"></div>
+<h1>テスト</h1>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/vegas/2.4.4/vegas.min.js"></script>
 <script src="../js/slide.js"></script>
