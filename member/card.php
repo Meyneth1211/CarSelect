dd06f4645718fd0dec2f14d283b57b11dfbb3055
murakami722 <2301208@s.asojuku.ac.jp>
2024-11-08 12:15:28 +0900

    Update card.php

--- member/card.php
+++ member/card.php
@@ -7,7 +7,9 @@
     <p>セキュリティコード<input class="card-cord"type="text" name="cardpass" maxlength="3" size="5"></p>
     <p>カード名義<input class="card-name" type="text" name="cardname" maxlength="50"></p>
     <p>有効期限<input class="card-kigen" type="text" name="cardkigen" maxlength="2" size="4">/<input type="text" maxlength="2" size="4"></p>
+    <div class="c-button">
     <p><input class="card-button" type="submit" onclick="location.href='./kounyuu.html'" value="送信"></p>
+    </div>
     </form>
         </div>
     </div>
