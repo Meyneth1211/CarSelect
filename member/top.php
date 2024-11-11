d8c8b674a40589102d2e44b107585a956aa3c9f1
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-11 11:01:58 +0900

    css追加

--- member/top.php
+++ member/top.php
@@ -36,7 +36,7 @@
         foreach($error as $e){
             echo $e.'<br>';
         }
-        echo '<div class="e"><form class="login-form" action="top.php" method="post"><input class="button-1" type="submit" value="戻る"></form></div></div></div>';
+        echo '<form class="login-form" action="top.php" method="post"><input class="button-1" type="submit" value="戻る"></form></div></div>';
         exit;
     }
 ?>
