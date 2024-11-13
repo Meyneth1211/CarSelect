f9eb953ac935fe118b68c04dc889c182d96df26f
murakami722 <2301208@s.asojuku.ac.jp>
2024-11-13 12:42:37 +0900

    card
    card

--- member/card.php
+++ member/card.php
@@ -5,7 +5,7 @@
     <h2>カード情報</h2>
     <div class="p-color">
     <p class="mclass">カード番号<input class="card-no"type="text" name="cardno" maxlength="16"></p>
-    <p class="mclass">セキュリティコード<input class="card-cord" type="password" placeholder="Password" name="cardpass" maxlength="3" size="5"></p>
+    <p class="mclass">セキュリティコード<input class="card-cord" type="password" name="cardpass" maxlength="3" size="5"></p>
     <p class="mclass">カード名義<input class="card-name" type="text" name="cardname" maxlength="50"></p>
     <p class="mclass">有効期限<input class="card-kigen" type="text" name="cardkigen1" maxlength="2" size="4">/<input class="card-kigen2" type="text" maxlength="2" size="4"></p>
     </div>
