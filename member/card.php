ce6545b48e1b5ff6adeaf0c119d4bd7c1f2b596a
murakami722 <2301208@s.asojuku.ac.jp>
2024-11-13 10:34:12 +0900

    card
    cardkigen henkou

--- member/card.php
+++ member/card.php
@@ -7,7 +7,7 @@
     <p class="mclass">カード番号<input class="card-no"type="text" name="cardno" maxlength="16"></p>
     <p class="mclass">セキュリティコード<input class="card-cord"type="text" name="cardpass" maxlength="3" size="5"></p>
     <p class="mclass">カード名義<input class="card-name" type="text" name="cardname" maxlength="50"></p>
-    <p class="mclass">有効期限<input class="card-kigen" type="text" name="cardkigen" maxlength="2" size="4">/<input class="card-kigen" type="text" maxlength="2" size="4"></p>
+    <p class="mclass">有効期限<input class="card-kigen" type="text" name="cardkigen1" maxlength="2" size="4">/<input class="card-kigen2" type="text" maxlength="2" size="4"></p>
     </div>
     <div class="c-button">
     <p><input class="card-button" type="submit" onclick="location.href='./kounyuu.html'" value="送信"></p>
