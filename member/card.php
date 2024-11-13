231e2dde8cc056f21a05cf545704852974fe19f1
murakami722 <2301208@s.asojuku.ac.jp>
2024-11-13 09:47:52 +0900

    card.php
    card

--- member/card.php
+++ member/card.php
@@ -4,10 +4,10 @@
     <form class="card-form" action="">
     <h2>カード情報</h2>
     <div class="p-color">
-    <p>カード番号   <input class="card-no"type="text" name="cardno" maxlength="16"></p>
-    <p>セキュリティコード        <input class="card-cord"type="text" name="cardpass" maxlength="3" size="5"></p>
-    <p>カード名義   <input class="card-name" type="text" name="cardname" maxlength="50"></p>
-    <p>有効期限       <input class="card-kigen" type="text" name="cardkigen" maxlength="2" size="4">/<input class="card-kigen" type="text" maxlength="2" size="4"></p>
+    <p>カード番号<input class="card-no"type="text" name="cardno" maxlength="16"></p>
+    <p>セキュリティコード<input class="card-cord"type="text" name="cardpass" maxlength="3" size="5"></p>
+    <p>カード名義<input class="card-name" type="text" name="cardname" maxlength="50"></p>
+    <p>有効期限<input class="card-kigen" type="text" name="cardkigen" maxlength="2" size="4">/<input class="card-kigen" type="text" maxlength="2" size="4"></p>
     </div>
     <div class="c-button">
     <p><input class="card-button" type="submit" onclick="location.href='./kounyuu.html'" value="送信"></p>
