e006c1c57133a2c4ae2fe0331519c16b0ea1c90a
murakami722 <2301208@s.asojuku.ac.jp>
2024-11-13 10:47:44 +0900

    カードphp
    カードの名前変更

--- member/kounyuu.php
+++ member/kounyuu.php
@@ -1,4 +1,4 @@
 <body>
-    <input type="button" onclick="location.href='./kounyuu.html'" value="戻る">
-    <input type="button" onclick="location.href='./kounyuu.html'" value="購入確定">
+    <input type="button" onclick="location.href='./kounyuu.php'" value="戻る">
+    <input type="button" onclick="location.href='./kounyuu.php'" value="購入確定">
 </body>
