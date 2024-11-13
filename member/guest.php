d6915b5ff8c27ffc35a3b5f9d973f46c203b976f
murakami722 <2301208@s.asojuku.ac.jp>
2024-11-13 12:31:53 +0900

    guest
    guest henkou

--- member/guest.php
+++ member/guest.php
@@ -2,16 +2,13 @@
 </body>
     <div class="guestcreate">
         <div class="guestcreate-card">
-            <h2>新規会員登録</h2>
+            <h2>ゲスト登録</h2>
             <form class="guestcreate-form" action="" method="post">
                 <input class="guestname" name="name" type="text" placeholder="name">
                 <input class="guestmail" name="mail" type="email" placeholder="Email">
                 <input class="guestaddress" name="address" type="text" placeholder="address">
                 <div class="guesttop"></div>
-                <input class="guestbutton-1" type="submit" value="登録">
-            </form>
-            <form class="guestcreate-form" action="guest.php" method="post">
-                    <input class="guestbutton-2" type="submit" value="ログイン画面へ戻る">
+                <input class="guestbutton" type="submit" value="登録">
             </form>
         </div>
     </div>
