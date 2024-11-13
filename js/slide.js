d134e1d81e741cda7b39737b890d86528fe2a05a
shimityoko9 <2401333@s.asojuku.ac.jp>
2024-11-14 04:10:58 +0900

    プログラム変更

--- js/slide.js
+++ js/slide.js
@@ -1,29 +1,21 @@
-//画像の設定
+$('.slider').slick({
+	fade:true,//切り替えをフェードで行う。初期値はfalse。
+	autoplay: true,//自動的に動き出すか。初期値はfalse。
+	autoplaySpeed: 3000,//次のスライドに切り替わる待ち時間
+	speed:1000,//スライドの動きのスピード。初期値は300。
+	infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+	slidesToShow: 1,//スライドを画面に3枚見せる
+	slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+	arrows: true,//左右の矢印あり
+	prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+	nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+	dots: true,//下部ドットナビゲーションの表示
+	pauseOnFocus: false,//フォーカスで一時停止を無効
+	pauseOnHover: false,//マウスホバーで一時停止を無効
+	pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
+});
 
-var windowwidth = window.innerWidth || document.documentElement.clientWidth || 0;
-		if (windowwidth > 768){
-			var responsiveImage = [//PC用の画像
-				{ src: '../img/slide5.jpg'},
-				{ src: '../img/slide6.jpg'},
-				{ src: '../img/slide8.jpg'}
-			];
-		} else {
-			var responsiveImage = [//タブレットサイズ（768px）以下用の画像
-				{ src: '../img/img_sp_01.jpg' },
-				{ src: '../img/img_sp_02.jpg' },
-				{ src: '../img/img_sp_03.jpg' }
-			];
-		}
-
-//Vegas全体の設定
-
-$('#slider').vegas({
-		overlay: true,//画像の上に網線やドットのオーバーレイパターン画像を指定。
-		transition: 'fade2',//切り替わりのアニメーション。http://vegas.jaysalvat.com/documentation/transitions/参照。fade、fade2、slideLeft、slideLeft2、slideRight、slideRight2、slideUp、slideUp2、slideDown、slideDown2、zoomIn、zoomIn2、zoomOut、zoomOut2、swirlLeft、swirlLeft2、swirlRight、swirlRight2、burnburn2、blurblur2、flash、flash2が設定可能。
-		transitionDuration: 2000,//切り替わりのアニメーション時間をミリ秒単位で設定
-		delay: 5000,//スライド間の遅延をミリ秒単位で。
-		animationDuration: 20000,//スライドアニメーション時間をミリ秒単位で設定
-		animation: 'kenburns',//スライドアニメーションの種類。http://vegas.jaysalvat.com/documentation/transitions/参照。kenburns、kenburnsUp、kenburnsDown、kenburnsRight、kenburnsLeft、kenburnsUpLeft、kenburnsUpRight、kenburnsDownLeft、kenburnsDownRight、randomが設定可能。
-		slides: responsiveImage,//画像設定を読む
-		//timer:false,// プログレスバーを非表示したい場合はこのコメントアウトを外してください
-	});
+//スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
+$('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
+$('.slider').slick('slickPlay');
+});
