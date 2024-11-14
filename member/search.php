d7a3f50f1c909ec721b3b78c97859a20f3d87d2b
shimityoko9 <2401333@s.asojuku.ac.jp>
2024-11-15 05:14:16 +0900

    Update search.php

--- member/search.php
+++ member/search.php
@@ -110,19 +110,19 @@ if (isset($_POST['daihatu_x'])) {
   <form method="body-type" action="">
     <label>
       <input type="checkbox" name="body-type[]" value="Sedan">
-      <img src="../img/セダン.png" alt="セダン" width="40px">
+      <img src="../img/セダン.png" alt="セダン" width="30px">
     </label>
     <label>
       <input type="checkbox" name="body-type[]" value="SUV">
-      <img src="../img/SUV.png" alt="SUV" width="40px">
+      <img src="../img/SUV.png" alt="SUV" width="30px">
     </label>
     <label>
       <input type="checkbox" name="body-type[]" value="Wagon">
-      <img src="../img/トラック.png" alt="ワゴン" width="30px">
+      <img src="../img/トラック.png" alt="ワゴン" width="25px">
     </label>
     <label>
       <input type="checkbox" name="body-type[]" value="Conpact">
-      <img src="../img/ワンボックスカー.png" alt="コンパクト" width="30px">
+      <img src="../img/ワンボックスカー.png" alt="コンパクト" width="23x">
     </label>
     <label>
       <input type="checkbox" name="body-type[]" value="Light">
