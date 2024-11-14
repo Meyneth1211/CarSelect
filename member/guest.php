3813ababf172da3a4b8472d61fc61f6789fe0a28
murakami722 <2301208@s.asojuku.ac.jp>
2024-11-14 10:07:38 +0900

    guest.php
    requierd

--- member/guest.php
+++ member/guest.php
@@ -3,10 +3,10 @@
     <div class="guestcreate">
         <div class="guestcreate-card">
             <h2>ゲスト登録</h2>
-            <form class="guestcreate-form" action="card.php" method="post">
-                <input class="guestname" name="name" type="text" placeholder="name">
-                <input class="guestmail" name="mail" type="email" placeholder="Email">
-                <input class="guestaddress" name="address" type="text" placeholder="address">
+            <form class="guestcreate-form" action="card.php" method="post" required>
+                <input class="guestname" name="name" type="text" placeholder="name" required>
+                <input class="guestmail" name="mail" type="email" placeholder="Email" required>
+                <input class="guestaddress" name="address" type="text" placeholder="address" required>
                 <div class="guesttop"></div>
                 <input class="guestbutton" type="submit" value="登録">
             </form>
