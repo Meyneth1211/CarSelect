d0ca2d37c4c499a7283abb8b80d191aa30a4d13b
shimityoko9 <2401333@s.asojuku.ac.jp>
2024-11-15 04:31:13 +0900

    pricelabelとbuttonElementを追加

--- js/search.js
+++ js/search.js
@@ -1,11 +1,12 @@
-function selectPrice(id) {
+function selectPrice(id, priceLabel, buttonElement) {
     // すべてのラジオボタンを解除
     document.querySelectorAll('.price-range input[type="radio"]').forEach((radio) => {
       radio.checked = false;
     });
 
-    // 指定されたIDのラジオボタンをチェックする！
-    document.getElementById(id).checked = true;
+    // 指定されたIDのラジオボタンをチェック!
+    const selectedRadio = document.getElementById(id);
+    selectedRadio.checked = true;
 
     // すべてのボタンから「selected」クラスを削除
     document.querySelectorAll('.price-range button').forEach((button) => {
@@ -13,5 +14,8 @@ function selectPrice(id) {
     });
 
     // 選択されたボタンに「selected」クラスを追加
-    document.querySelector(`button[onclick="selectPrice('${id}')"]`).classList.add('selected');
+    buttonElement.classList.add('selected');
+
+    // 選択された価格を表示
+    document.getElementById('selected-price').innerText = `選択された価格: ${priceLabel}`;
   }
