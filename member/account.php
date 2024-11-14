25111ce7883e22d80d57f9c638b8c6099831246e
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-14 10:01:54 +0900

    ぢvタグ追加

--- member/account.php
+++ member/account.php
@@ -24,6 +24,8 @@ if (isset($_SESSION['user_id'])) {
         echo 'アカウント情報が見つかりませんでした。';
     }
 } else {
+    echo '<div class="error-back"><div class="error-card">';
     echo 'ログインしてください。';
+    echo '<form class="login-form" action="login.php" method="post"><input class="button-1" type="submit" value="ログインする"></form></div></div>';
 }
 ?>
