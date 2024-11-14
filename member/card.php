fcd19671925df93e23db4eb1fdc1646cce18fa11
murakami722 <2301208@s.asojuku.ac.jp>
2024-11-14 10:11:47 +0900

    card.php
    requi

--- member/card.php
+++ member/card.php
@@ -4,10 +4,10 @@
     <form class="card-form" action="">
     <h2>カード情報</h2>
     <div class="p-color">
-    <p class="mclass">カード番号<input class="card-no"type="text" name="cardno" maxlength="16"></p>
-    <p class="mclass">セキュリティコード<input class="card-cord" type="password" name="cardpass" maxlength="3" size="5"></p>
-    <p class="mclass">カード名義<input class="card-name" type="text" name="cardname" maxlength="50"></p>
-    <p class="mclass">有効期限<input class="card-kigen" type="text" name="cardkigen1" maxlength="2" size="4">/<input class="card-kigen2" type="text" maxlength="2" size="4"></p>
+    <p class="mclass">カード番号<input class="card-no"type="text" name="cardno" maxlength="16" required></p>
+    <p class="mclass">セキュリティコード<input class="card-cord" type="password" name="cardpass" maxlength="3" size="5" required></p>
+    <p class="mclass">カード名義<input class="card-name" type="text" name="cardname" maxlength="50" required></p>
+    <p class="mclass">有効期限<input class="card-kigen" type="text" name="cardkigen1" maxlength="2" size="4" required>/<input class="card-kigen2" type="text" maxlength="2" size="4" required></p>
     </div>
     <div class="c-button">
     <p><input class="card-button" type="submit" onclick="location.href='./kounyuu.php'" value="送信"></p>
