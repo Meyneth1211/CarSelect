058a3ed05ddfac9a913d06e89696177e26a1aa26
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-14 10:05:55 +0900

    タグ

--- member/account.php
+++ member/account.php
@@ -25,7 +25,7 @@ if (isset($_SESSION['user_id'])) {
     }
 } else {
     echo '<div class="error-back"><div class="error-card">';
-    echo '<div class="account-message">ログインしてください。</div>';
+    echo '<div class="account-message3">ログインしてください。</div>';
     echo '<form class="login-form" action="login.php" method="post"><input class="button-1" type="submit" value="ログインする"></form></div></div>';
 }
 ?>
