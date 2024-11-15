482c38d23b94d13c97e3615de440b63577ec8185
murakami722 <2301208@s.asojuku.ac.jp>
2024-11-15 11:56:43 +0900

    購入
    変更

--- member/kounyuu.php
+++ member/kounyuu.php
@@ -1,7 +1,9 @@
 <?php require('../header/header.php'); ?>
 <div class="kounyuu">
-<form class="kounyuu-form" action="kounyuu_kanryou.php" method="post">
-    <input class="kounyuu-button1" type="submit" value="戻る">
-    <input class="kounyuu-button2" type="submit" value="購入確定">
-</form>
+    <div class="kounyuu-card">
+    <form class="kounyuu-form" action="kounyuu_kanryou.php" method="post">
+        <input class="kounyuu-button1" type="submit" value="戻る">
+        <input class="kounyuu-button2" type="submit" value="購入確定">
+    </form>
+</div>
 </div>
