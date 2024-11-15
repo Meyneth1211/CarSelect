c051d92e43637e39030385081b26fdec05c389ae
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-15 12:07:52 +0900

    Update search.php

--- member/search.php
+++ member/search.php
@@ -132,7 +132,7 @@ if (isset($_POST['daihatu_x'])) {
   </div>
 
   <!-- カラー -->
-  <div class="color-title">カラー</div>
+  <div class="title">カラー</div>
   <div class="color-options">
     <form method="color" action="">
       <label><input type="checkbox" name="color[]" value="Black"></label>
