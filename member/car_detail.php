ae9f9cbf514922e86773d20be641d7a3c155c1fc
Meyneth1211 <2301389@s.asojuku.ac.jp>
2024-11-18 22:41:56 +0900

    データベースからの画像表示テスト
    商品詳細画面における商品画像を動的にデータベースから取得して表示する機能のテスト実装

--- member/car_detail.php
+++ member/car_detail.php
@@ -6,6 +6,23 @@
     <title>車の詳細 - Car Select</title>
 </head>
 <body>
+    <!-- ヘッダ呼び出し -->
     <?php require('../header/header.php'); ?>
+
+    <div class="car_image">
+        <?php
+            require_once '../DBconnect.php';
+            $pdo=getDB();
+            $item=$_GET['item'];
+            $sql='select image from image where car_id = ? and is_primary = 1';
+            $stmt=$pdo->prepare($sql);
+            $stmt->execute([$item]);
+            $output=$stmt->fetch();
+            foreach ($stmt as $row) {
+                echo '<img src="'. $row['image']. '">';
+            }
+            $pdo=null;
+        ?>
+    </div>
 </body>
 </html>
