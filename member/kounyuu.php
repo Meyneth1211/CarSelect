1ad5060642cb0120b8606df49b54c3eed95546dc
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-18 10:27:28 +0900

    修正

--- member/kounyuu.php
+++ member/kounyuu.php
@@ -1,11 +1,11 @@
 <?php require('../header/header.php'); ?>
-<div class="kounyuu">
-    <div class="kounyuu-card">
-    <form class="kounyuu-form" action="" method="post">
-        <input class="kounyuu-button1" type="submit" value="戻る">
-        <input class="kounyuu-button2" type="submit" value="購入確定">
-    </form>
-</div>
-</div>
+    <div class="kounyuu">
+        <div class="kounyuu-card">
+            <form class="kounyuu-form" action="" method="post">
+                <input class="button1" type="submit" value="戻る">
+                <input class="button2" type="submit" value="購入確定">
+            </form>
+        </div>
+    </div>
 </body>
 </html>
