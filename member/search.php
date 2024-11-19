c4df6349f3fbeb261efb79a7ade6976ac1820a9d
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-19 22:13:09 +0900

    値段変更

--- member/search.php
+++ member/search.php
@@ -51,19 +51,19 @@
 <div class="price-range">
   <form method="post" action="">
     <input type="radio" name="price" value="30" id="price-30" hidden>
-    <button type="button" onclick="selectPrice('price-30', '～30万円', this)">～30万円</button>
+    <button type="button" onclick="selectPrice('price-30', '～400万円', this)">～30万円</button>
 
     <input type="radio" name="price" value="50" id="price-50" hidden>
-    <button type="button" onclick="selectPrice('price-50', '～50万円', this)">～50万円</button>
+    <button type="button" onclick="selectPrice('price-50', '～600万円', this)">～50万円</button>
 
     <input type="radio" name="price" value="100" id="price-100" hidden>
-    <button type="button" onclick="selectPrice('price-100', '～100万円', this)">～100万円</button>
+    <button type="button" onclick="selectPrice('price-100', '～800万円', this)">～100万円</button>
 
     <input type="radio" name="price" value="200" id="price-200" hidden>
-    <button type="button" onclick="selectPrice('price-200', '～200万円', this)">～200万円</button>
+    <button type="button" onclick="selectPrice('price-200', '～1000万円', this)">～200万円</button>
 
     <input type="radio" name="price" value="200over" id="price-200over" hidden>
-    <button type="button" onclick="selectPrice('price-200over', '200万円～', this)">200万円～</button>
+    <button type="button" onclick="selectPrice('price-200over', '1000万円～', this)">200万円～</button>
   </form>
 </div>
 <!-- もし価格が選択されていなければこれを表示 -->
