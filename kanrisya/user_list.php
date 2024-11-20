e43086e9facd9ea6d88a387dab5156eee2ffe166
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-20 12:22:00 +0900

    削除機能追加

--- kanrisya/user_list.php
+++ kanrisya/user_list.php
@@ -7,21 +7,11 @@ $pdo = getDb();
 <h1>ユーザー削除画面です！</h1>
 
 <?php
-// 削除処理
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete_user_mail'])) {
-    $deleteMail = $_POST['delete_user_mail'];
-    $deleteSql = $pdo->prepare('DELETE FROM user WHERE user_mail = ?');
-    $deleteResult = $deleteSql->execute([$deleteMail]);
-    if ($deleteResult) {
-        echo '<p>ユーザーが正常に削除されました。</p>';
-    } else {
-        echo '<p>ユーザーの削除に失敗しました。</p>';
-    }
-}
+
 
 // ユーザー一覧の表示
 $sql = $pdo->query('SELECT user_name, user_mail, user_address FROM user');
-echo '<form method="post" action="">';
+echo '<form method="post" action="user_delete.php">';
 echo '<table border="1">';
 echo '<tr><th>名前</th><th>メールアドレス</th><th>住所</th><th>ユーザ削除ボタン</th></tr>';
 foreach ($sql as $row) {
