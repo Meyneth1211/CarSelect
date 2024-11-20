325a9bb7b492444ea93907df9fca2f934ed7215f
shimityoko9 <2401333@s.asojuku.ac.jp>
2024-11-20 11:37:25 +0900

    追加

--- kanrisya/kanrisya_signup.php
+++ kanrisya/kanrisya_signup.php
@@ -7,7 +7,6 @@
             <form class="create-form" action="signup_backend.php" method="post">
                 <input class="mail" name="mail" type="email" placeholder="Email" required>
                 <input class="pass" name="pass" type="password" placeholder="Password" required>
-                <div class="top"></div>
                 <input class="button-1" type="submit" value="登録">
             </form>
             <form class="create-form" action="login.php" method="post">
