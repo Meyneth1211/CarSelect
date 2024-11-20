28c351a6a05ee663a31695a32356715e21e770ee
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-20 10:57:45 +0900

    トップページボタン追加

--- kanrisya/kanrisya_top.php
+++ kanrisya/kanrisya_top.php
@@ -1,6 +1,10 @@
 <?php require('kanrisya_header.php'); ?>
 
 <input type="button" onclick="location.href='user_delete'" value="ユーザー削除">
+<input type="button" onclick="location.href='#'" value="管理者登録">
+<input type="button" onclick="location.href='#'" value="在庫管理">
+<input type="button" onclick="location.href='#'" value="商品追加">
+
 
 </body>
 </html>
---
+++ kanrisya/user_delete.php
@@ -0,0 +1,3 @@
+<?php require('kanrisya_header.php'); ?>
+
+<h1>ユーザー削除画面です！</h1>
