706db11046ad0f435ae768760137f60df8854165
murakami722 <2301208@s.asojuku.ac.jp>
2024-11-20 10:42:39 +0900

    modoru

--- member/kounyuu.php
+++ member/kounyuu.php
@@ -2,7 +2,7 @@
     <div class="kounyuu">
         <div class="kounyuu-card">
             <form class="kounyuu-form" action="Purchase_confirmed.php" method="post">
-                <input class="kounyuu-button1" type="submit" value="戻る">
+                <input class="kounyuu-button1" onclick="location:href='kounyuu.php'" type="submit" value="戻る">
                 <input class="kounyuu-button2" type="submit" value="購入確定">
             </form>
         </div>
