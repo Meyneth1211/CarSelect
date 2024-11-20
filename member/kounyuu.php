2ebf261c1c13591b6668bfc3b2ee9e2fe0ffd1bf
murakami722 <2301208@s.asojuku.ac.jp>
2024-11-20 10:46:10 +0900

    ko

--- member/kounyuu.php
+++ member/kounyuu.php
@@ -1,8 +1,10 @@
 <?php require('../header/header.php'); ?>
     <div class="kounyuu">
         <div class="kounyuu-card">
-            <form class="kounyuu-form" action="Purchase_confirmed.php" method="post">
-                <input class="kounyuu-button1" onclick="location:href='./kounyuu.php'" type="submit" value="戻る">
+            <form class="kounyuu1-form" action="Purchase_confirmed.php" method="post">
+                <input class="kounyuu-button1" type="submit" value="戻る">
+            </form>
+            <form class="kounyuu2-form" action="Purchase_confirmed.php" method="post">
                 <input class="kounyuu-button2" type="submit" value="購入確定">
             </form>
         </div>
