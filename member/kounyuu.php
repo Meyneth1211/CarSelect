5bdffe7c2fb12d94dfc0eccf61e9317fa2d2792e
murakami722 <2301208@s.asojuku.ac.jp>
2024-11-20 09:47:58 +0900

    kounyuu
    kounyuu new

--- member/kounyuu.php
+++ member/kounyuu.php
@@ -2,8 +2,8 @@
     <div class="kounyuu">
         <div class="kounyuu-card">
             <form class="kounyuu-form" action="" method="post">
-                <input class="button1" type="submit" value="戻る">
-                <input class="button2" type="submit" value="購入確定">
+                <input class="kounyuu-button1" type="submit" value="戻る">
+                <input class="kounyuu-button2" type="submit" value="購入確定">
             </form>
         </div>
     </div>
