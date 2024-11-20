099485d05892cae81ac7790cf8304c45c2a87575
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-20 12:43:35 +0900

    css追加

--- kanrisya/kanrisya_top.php
+++ kanrisya/kanrisya_top.php
@@ -1,10 +1,10 @@
 <?php require('kanrisya_header.php'); ?>
-
-<input type="button" onclick="location.href='user_list'" value="ユーザー削除">
-<input type="button" onclick="location.href='kanrisya_signup'" value="管理者登録">
-<input type="button" onclick="location.href='#'" value="在庫管理">
-<input type="button" onclick="location.href='#'" value="商品追加">
-
+<div class="top_button">
+    <input type="button" onclick="location.href='user_list'" value="ユーザー削除">
+    <input type="button" onclick="location.href='kanrisya_signup'" value="管理者登録">
+    <input type="button" onclick="location.href='#'" value="在庫管理">
+    <input type="button" onclick="location.href='#'" value="商品追加">
+</div>
 
 </body>
 </html>
