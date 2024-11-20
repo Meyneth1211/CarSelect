5653852121dfd6d6ed3628f79b961efdae6200e9
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-20 11:42:15 +0900

    user削除画面（ユーザーの一覧を表示する）

--- kanrisya/user_delete.php
+++ kanrisya/user_delete.php
@@ -1,5 +1,24 @@
-<?php require('kanrisya_header.php'); ?>
+<?php 
+    require('kanrisya_header.php'); 
+    require_once '../DBconnect.php';
+    $pdo = getDb();
+?>
 
 <h1>ユーザー削除画面です！</h1>
+
+<?php
+    $sql = $pdo->query('SELECT user_name, user_mail, user_address FROM user');
+    echo '<table border="1">';
+    echo '<tr><th>名前</th><th>メールアドレス</th><th>住所</th></tr>';
+    foreach ($sql as $row) {
+        echo '<tr>';
+        echo '<td>' . $row['user_name'] . '</td>';
+        echo '<td>' . $row['user_mail'] . '</td>';
+        echo '<td>' . $row['user_address'] . '</td>';
+        echo '</tr>';
+    }
+    echo '</table>';
+    ?>
+?>
 </body>
 </html>
