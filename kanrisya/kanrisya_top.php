14d069003a8cd151a061a1cee2ab09a956c5c9e4
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-21 21:35:15 +0900

    トップ。phpセッション追加

--- kanrisya/kanrisya_top.php
+++ kanrisya/kanrisya_top.php
@@ -1,4 +1,31 @@
-<?php require('kanrisya_header.php'); ?>
+<?php 
+    session_start();
+    require('kanrisya_header.php'); 
+    require_once '../DBconnect.php';
+    $pdo = getDb();
+
+    //メールアドレスとパスワードが入力されているか確認
+    if(!empty($_POST['mail'])&&!empty($_POST['pass'])){
+        $sql=$pdo->prepare('SELECT * from user where admin_mail=? and admin_password=?');
+        $sql->execute([$_POST['mail'],$_POST['pass']]);
+        $user = $sql->fetch(PDO::FETCH_ASSOC);
+
+        // ユーザーが存在する場合
+        if ($user) {
+            $_SESSION['user_id'] = $user['admin_id']; // セッションにユーザーIDを保存
+            $_SESSION['mail'] = $user['admin_mail'];
+            $_SESSION['name'] = $user['admin_name'];
+        } else {
+            // ログイン失敗時のエラーメッセージ表示
+            echo '<div class="container"><div class="message success">EmailかPasswordが違います</div>';
+            echo '<a class="return-button" href="login.php">戻る</a></div>';
+            exit;
+        }
+    }
+    $pdo = null;
+?>
+
+
 <div class="top_button">
     <input class="top_button1" type="button" onclick="location.href='user_list'" value="ユーザー削除">
     <input class="top_button2" type="button" onclick="location.href='kanrisya_signup'" value="管理者登録">
