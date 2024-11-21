cb34675fa3f59e5da6072f7d5c06ea3f882d2435
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-22 00:35:36 +0900

    トップ遷移先変更

--- kanrisya/kanrisya_top.php
+++ kanrisya/kanrisya_top.php
@@ -4,7 +4,7 @@
     <input class="top_button1" type="button" onclick="location.href='user_list'" value="ユーザー削除">
     <input class="top_button2" type="button" onclick="location.href='kanrisya_signup'" value="管理者登録">
     <input class="top_button3" type="button" onclick="location.href='car_list'" value="在庫管理">
-    <input class="top_button4" type="button" onclick="location.href='#'" value="商品追加">
+    <input class="top_button4" type="button" onclick="location.href='kanrisya_insert'" value="商品追加">
 </div>
 
 </body>
