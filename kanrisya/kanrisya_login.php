7263f58e595565700c5d35ce95231de5c3167c22
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-21 20:07:36 +0900

    管理者ログイン画面追加

---
+++ kanrisya/kanrisya_login.php
@@ -0,0 +1,18 @@
+<?php require('kanrisya_header.php'); ?>
+
+<div class="kanrisya_login">
+        <div class="kanrisya_login_card">
+            <h2>ログイン</h2>
+            <form class="kanrisya_login_form" action="top.php" method="post">
+                <input class="mail" name="mail" type="text" placeholder="Email">
+                <input class="pass" name="pass" type="password" placeholder="Password">
+                <div class="top"></div>
+                <input class="kanrisya_button_1" type="submit" value="ログイン">
+            </form>
+            <form class="kanrisya_login_form" action="signup.php" method="post">
+                    <input class="kanrisya_button_2" type="submit" value="アカウント新規作成">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
