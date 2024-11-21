9c1e24aee1656da279963829ca85209916a0a5aa
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-21 14:49:37 +0900

    Update car_delete.php

--- kanrisya/car_delete.php
+++ kanrisya/car_delete.php
@@ -2,31 +2,34 @@
 require('kanrisya_header.php'); 
 require_once '../DBconnect.php';
 $pdo = getDb();
+?>
+    <div class="container">
+    <?php
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete_id'])) {
+            // 削除するcar_idを取得
+            $deleteId = $_POST['delete_id'];
 
+            // 削除クエリを準備
+            $deleteSql = $pdo->prepare('DELETE FROM car WHERE car_id = ?');
+            $deleteResult = $deleteSql->execute([$deleteId]);
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete_id'])) {
-        // 削除するcar_idを取得
-        $deleteId = $_POST['delete_id'];
-
-        // 削除クエリを準備
-        $deleteSql = $pdo->prepare('DELETE FROM car WHERE car_id = ?');
-        $deleteResult = $deleteSql->execute([$deleteId]);
-
-        if ($deleteResult) {
-            // 削除成功時
-            echo '<p style="color: green;">削除完了しました。</p>';
+            if ($deleteResult) {
+                // 削除成功時
+                echo '<div class="message success">商品が正常に削除されました。</div>';
+            } else {
+                // 削除失敗時
+                echo '<div class="message error">商品の削除に失敗しました。</div>';
+            }
         } else {
-            // 削除失敗時
-            echo '<p style="color: red;">削除に失敗しました。</p>';
+            // 不正なアクセス時の処理
+            echo '<p style="color: red;">不正なアクセスです。</p>';
         }
-    } else {
-        // 不正なアクセス時の処理
-        echo '<p style="color: red;">不正なアクセスです。</p>';
-    }
-    ?>
-    
-    <input type="button" onclick="location.href='car_list'" value="一覧画面へ戻る">
-    <input type="button" onclick="location.href='kanrisya_top'" value="トップページへ戻る">
+        ?>
 
+        <div class="button-group">
+            <input type="button" onclick="location.href='car_list'" value="一覧画面へ戻る">
+            <input type="button" onclick="location.href='kanrisya_top'" value="トップページへ戻る">
+    </div>
+    </div>
 </body>
 </html>
