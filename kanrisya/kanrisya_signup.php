8d63fa1f89981af814d7cf20a9fbf728a9bd13bd
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-21 20:32:40 +0900

    タグの位置変更

--- kanrisya/kanrisya_signup.php
+++ kanrisya/kanrisya_signup.php
@@ -7,9 +7,8 @@
             <form class="kanrisya_login_form" action="kanrisya_signup_backend.php" method="post">
                 <input class="mail" name="mail" type="email" placeholder="Email" required>
                 <input class="pass" name="pass" type="password" placeholder="Password" required>
-                <div class="kanrisya_margin_top">
-                    <input class="kanrisya_button_1" type="submit" value="登録">
-                </div>
+                <div class="kanrisya_margin_top"></div>
+                <input class="kanrisya_button_1" type="submit" value="登録">
             </form>
             <form class="kanrisya_login_form" action="kanrisya_login.php" method="post">
                     <input class="kanrisya_button_2" type="submit" value="ログイン画面へ戻る">
