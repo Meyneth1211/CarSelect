2a358b682cb401846682c326c17ab62aa216b8c4
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-21 20:23:08 +0900

    管理者新規会員登録画面クラス名変更。ログイン画面のcss再利用

--- kanrisya/kanrisya_signup.php
+++ kanrisya/kanrisya_signup.php
@@ -1,16 +1,16 @@
 <?php require('../kanrisya/kanrisya_header.php'); 
 ?>
 </body>
-    <div class="create">
-        <div class="create-card">
+    <div class="kanrisya_login">
+        <div class="kanrisya_login_card">
             <h2>管理者登録</h2>
-            <form class="create-form" action="kanrisya_signup_backend.php" method="post">
+            <form class="kanrisya_login_form" action="kanrisya_signup_backend.php" method="post">
                 <input class="mail" name="mail" type="email" placeholder="Email" required>
                 <input class="pass" name="pass" type="password" placeholder="Password" required>
-                <input class="button-1" type="submit" value="登録">
+                <input class="kanrisya_button_1" type="submit" value="登録">
             </form>
-            <form class="create-form" action="login.php" method="post">
-                    <input class="button-2" type="submit" value="ログイン画面へ戻る">
+            <form class="kanrisya_login_form" action="kanrisya_login.php" method="post">
+                    <input class="kanrisya_button_2" type="submit" value="ログイン画面へ戻る">
             </form>
         </div>
     </div>
