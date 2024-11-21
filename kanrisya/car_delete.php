23cc973b62658057a165c30962dc8838b26b2b22
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-21 10:17:42 +0900

    車削除機能、

---
+++ kanrisya/car_delete.php
@@ -0,0 +1,26 @@
+<?php
+require('kanrisya_header.php'); 
+require_once '../DBconnect.php';
+$pdo = getDb();
+
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete_id'])) {
+        // 削除するcar_idを取得
+        $deleteId = $_POST['delete_id'];
+
+        // 削除クエリを準備
+        $deleteSql = $pdo->prepare('DELETE FROM car WHERE car_id = ?');
+        $deleteResult = $deleteSql->execute([$deleteId]);
+
+        if ($deleteResult) {
+            // 削除成功時
+            echo '<p style="color: green;">削除完了しました。</p>';
+        } else {
+            // 削除失敗時
+            echo '<p style="color: red;">削除に失敗しました。</p>';
+        }
+    } else {
+        // 不正なアクセス時の処理
+        echo '<p style="color: red;">不正なアクセスです。</p>';
+    }
+    ?>
