e17c9a8af2d9f4364bc1071c398e35aa78528141
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-21 13:40:12 +0900

    ファイル名変更」

--- kanrisya/kanrisya_top.php
+++ kanrisya/kanrisya_top.php
@@ -2,7 +2,7 @@
 <div class="top_button">
     <input class="top_button1" type="button" onclick="location.href='user_list'" value="ユーザー削除">
     <input class="top_button2" type="button" onclick="location.href='kanrisya_signup'" value="管理者登録">
-    <input class="top_button3" type="button" onclick="location.href='zaiko_kanri'" value="在庫管理">
+    <input class="top_button3" type="button" onclick="location.href='car_list'" value="在庫管理">
     <input class="top_button4" type="button" onclick="location.href='#'" value="商品追加">
 </div>
 
