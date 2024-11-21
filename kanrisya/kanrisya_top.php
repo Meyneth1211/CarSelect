77417f2cee7f980696b9527ee51d7d8e2838e551
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-21 09:38:03 +0900

    Update kanrisya_top.php

--- kanrisya/kanrisya_top.php
+++ kanrisya/kanrisya_top.php
@@ -1,6 +1,6 @@
 <?php require('kanrisya_header.php'); ?>
 <div class="top_button">
-    <input type="button" onclick="location.href='user_list'" value="ユーザー削除">
+    <input class="" type="button" onclick="location.href='user_list'" value="ユーザー削除">
     <input type="button" onclick="location.href='kanrisya_signup'" value="管理者登録">
     <input type="button" onclick="location.href='#'" value="在庫管理">
     <input type="button" onclick="location.href='#'" value="商品追加">
