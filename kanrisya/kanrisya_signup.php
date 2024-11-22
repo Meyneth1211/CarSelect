ff03d473dfdb1f9d0adb66b7ded14e437f5dd6cb
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-22 15:21:41 +0900

    ログイン画面へ戻るを削除

--- kanrisya/kanrisya_signup.php
+++ kanrisya/kanrisya_signup.php
@@ -10,9 +10,6 @@
                 <div class="kanrisya_margin_top"></div>
                 <input class="kanrisya_button_1" type="submit" value="登録">
             </form>
-            <form class="kanrisya_login_form" action="kanrisya_first_login.php" method="post">
-                    <input class="kanrisya_button_2" type="submit" value="ログイン画面へ戻る">
-            </form>
             <form class="kanrisya_login_form" action="kanrisya_top.php" method="post">
                     <input class="kanrisya_button_2" type="submit" value="トップページへ戻る">
             </form>
