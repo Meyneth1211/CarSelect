a4df6224e82cf4cde6f36be16be4324d9e69ffe1
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-22 13:56:15 +0900

    サイズ変更

--- kanrisya/kanrisya_header.php
+++ kanrisya/kanrisya_header.php
@@ -17,7 +17,7 @@
 </head>
 <body>
     <div class="back">
-        <form action="kanrisya_logout.php" method="post"><input type="image" src="../img/logout.png"></form>
+        <form action="kanrisya_logout.php" method="post"><input type="image" src="../img/logout.png" width="50px" height="auto"></form>
         <h2><a class="logo_link" href="kanrisya_top">Car Select</a></h2>
     </div>
 
