c4a58bfbab443ddc8c84fd0b51d80df6cf4cf4bd
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-22 15:16:03 +0900

    トップページへ戻るボタン追加

--- kanrisya/kanrisya_login.php
+++ kanrisya/kanrisya_login.php
@@ -12,6 +12,9 @@
             <form class="kanrisya_login_form" action="kanrisya_signup.php" method="post">
                     <input class="kanrisya_button_2" type="submit" value="アカウント新規作成">
             </form>
+            <form class="kanrisya_login_form" action="kanrisya_top.php" method="post">
+                    <input class="kanrisya_button_2" type="submit" value="トップページへ戻る">
+            </form>
         </div>
     </div>
 </body>
