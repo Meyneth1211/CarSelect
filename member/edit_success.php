52bff6cb10b92574713fc89aa3db0bd404561402
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-23 20:06:14 +0900

    クラス名変更、css追加

--- member/edit_success.php
+++ member/edit_success.php
@@ -1,7 +1,7 @@
 <?php require('../header/header.php'); ?>
 
     <div class="account-error-back">
-        <div class="account-error-card">
+        <div class="account-error-card3">
             <h1 class="account_ok_messag">アカウント情報が正常に更新されました！</h1>
             <form class="login-form" action="account_update.php" method="post">
                 <input class="button-1" type="submit" value="戻る">
