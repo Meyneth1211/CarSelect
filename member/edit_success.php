051499af28b45a9475573ce1be2e154e84a69ea7
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-23 19:56:07 +0900

    文字白

--- member/edit_success.php
+++ member/edit_success.php
@@ -2,7 +2,7 @@
 
     <div class="error-back">
         <div class="error-card2">
-            <h1>アカウント情報が正常に更新されました！</h1>
+            <h1 class="account_ok_messag">アカウント情報が正常に更新されました！</h1>
             <form class="login-form" action="account_update.php" method="post">
                 <input class="button-1" type="submit" value="戻る">
             </form>
