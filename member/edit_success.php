a59a3b7841e08e01f5d041c4e318da2adf2f3861
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-23 17:50:02 +0900

    アカウント編集画面

---
+++ member/edit_success.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>更新成功</title>
+</head>
+<body>
+    <h1>ユーザー情報が正常に更新されました！</h1>
+    <a href="accountr.php">戻る</a>
+</body>
+</html>
