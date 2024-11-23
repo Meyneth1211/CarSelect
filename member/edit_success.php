e68f155d2ef67d8bb75d14ff0a696e71abd6a421
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-23 19:54:21 +0900

    クラス名定義

--- member/edit_success.php
+++ member/edit_success.php
@@ -1,6 +1,12 @@
 <?php require('../header/header.php'); ?>
 
-    <h1>アカウント情報が正常に更新されました！</h1>
-    <a href="account_update.php">編集ページに戻る</a>
+    <div class="error-back">
+        <div class="error-card2">
+            <h1>アカウント情報が正常に更新されました！</h1>
+            <form class="login-form" action="account_update.php" method="post">
+                <input class="button-1" type="submit" value="戻る">
+            </form>
+        </div>
+    </div>
 </body>
 </html>
