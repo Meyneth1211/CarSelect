f9d6680772140063b3aeb873903838b7bd07e349
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-23 20:13:14 +0900

    アカウント情報画面に名前を追加テスト

--- member/account.php
+++ member/account.php
@@ -9,13 +9,14 @@ if (isset($_SESSION['user_id'])) {
     $user_id = $_SESSION['user_id'];
 
     // ユーザー情報を取得
-    $sql = $pdo->prepare('SELECT user_mail, user_password, user_address FROM user WHERE user_id = ?');
+    $sql = $pdo->prepare('SELECT user_name, user_mail, user_password, user_address FROM user WHERE user_id = ?');
     $sql->execute([$user_id]);
     $user = $sql->fetch(PDO::FETCH_ASSOC);
 
     if ($user) {
         echo '<div class="account-back"><div class="account-card">';
         echo '<h2 class="account-message">アカウント情報</h2><br><br>';
+        echo '<p class="account-message2">名前:' . $user['user_name'] . '</p><br>';
         echo '<p class="account-message2">メールアドレス: ' . $user['user_mail'] . '</p><br>';
         echo '<p class="account-message2">パスワード: ' . $user['user_password'] . '</p><br>';
         echo '<p class="account-message2">住所: ' . $user['user_address'] . '</p><br>';
