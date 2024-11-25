46d655ed1bbafb9ba24805d0b79360443da5362a
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-25 15:49:33 +0900

    画像表示

--- kanrisya/image_edit.php
+++ kanrisya/image_edit.php
@@ -17,24 +17,17 @@ $sql->execute([$car_id]);
 $images = $sql->fetchAll();
 ?>
 
-<h1 class="page-title">画像編集画面</h1>
+<h1 class="page-title">画像一覧</h1>
 
 <table class="image-table">
     <tr>
         <th>画像</th>
-        <th>操作</th>
     </tr>
     <?php foreach ($images as $row): ?>
     <tr>
         <td>
-            <form method="post" action="update_image.php">
-                <button class="edit-button" type="submit" name="edit_image_id" value="<?= htmlspecialchars($row['image_id'], ENT_QUOTES, 'UTF-8') ?>">編集</button>
-            </form>
-        </td>
-        <td>
-            <form method="post" action="delete_image.php">
-                <button class="delete-button" type="submit" name="delete_image_id" value="<?= htmlspecialchars($row['image_id'], ENT_QUOTES, 'UTF-8') ?>">削除</button>
-            </form>
+            <!-- 画像の表示 -->
+            <img src="<?= htmlspecialchars($row['image'], ENT_QUOTES, 'UTF-8') ?>" alt="車両画像" class="car-image">
         </td>
     </tr>
     <?php endforeach; ?>
