865ebf5a2bc6af5d0c8ffd6147b77e4c9363a6ee
Meyneth1211 <2301389@s.asojuku.ac.jp>
2024-11-27 21:37:50 +0900

    Update search.php

--- member/search.php
+++ member/search.php
@@ -245,8 +245,8 @@ if (isset($_GET['s'])) {
 }elseif (isset($_GET['b'])) {
   echo 'トップページからのブランド検索リクエストを確認';
   $brand=$_GET['brand'];
-  $brand= "'". $brand . "'";
-  $sql = 'SELECT car_id, car_name, price FROM car WHERE brand = ?';
+  //$brand= "'". $brand . "'";
+  $sql = 'SELECT car_id, car_name, price FROM car WHERE brand = ? ;';
   require_once '../DBconnect.php';
   $pdo = getDB();
   $stmt = $pdo->prepare($sql);
