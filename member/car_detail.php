22be20ac0e75db6c74aa40aebbe65146d84e89be
Meyneth1211 <2301389@s.asojuku.ac.jp>
2024-11-28 02:27:01 +0900

    値渡し用コードの作成

--- member/car_detail.php
+++ member/car_detail.php
@@ -5,7 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>車の詳細 - Car Select</title>
-    <link rel="stylesheet" href="../css/style_for_car_detail.php?item=42" type="text/css">
+    <?php
+        $car=$_GET['item'];
+        echo '<link rel="stylesheet" href="../css/style_for_car_detail.php?item=' . $car .'" type="text/css">';
+    ?>
 </head>
 
 <body>
