f41ae1ed0855200a06cb2676b931e8b3bbd7e2e9
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-27 23:04:12 +0900

    購入ページ値受け渡しテスト

--- member/kounyuu.php
+++ member/kounyuu.php
@@ -1,4 +1,16 @@
 <?php require('../header/header.php'); ?>
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $car_id = $_POST['car_id'] ?? null;
+    if ($car_id) {
+        echo '受け取った car_id: ' . htmlspecialchars($car_id, ENT_QUOTES, 'UTF-8');
+        // car_id を利用した処理を記述
+    } else {
+        echo 'car_id が送信されていません。';
+    }
+}
+?>
+
     <div class="kounyuu">
         <div class="kounyuu-card">
             <form class="kounyuu1-form" action="car_detail.php" method="post">
