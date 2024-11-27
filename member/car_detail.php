3656dff49c0fbe6216eb4ecbf32326b357e497e5
Meyneth1211 <2301389@s.asojuku.ac.jp>
2024-11-28 02:00:59 +0900

    Update car_detail.php

--- member/car_detail.php
+++ member/car_detail.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>車の詳細 - Car Select</title>
-    <link rel="stylesheet" href="../css/style_for_car_detail.php?item=<?php $_GET['item']; ?>" type="text/css">
+    <link rel="stylesheet" href="../css/style_for_car_detail.php?item=42" type="text/css">
 </head>
 
 <body>
