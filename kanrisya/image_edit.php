664421af72b8c7001770e725a52ae8d9c99b0353
shimityoko9 <2401333@s.asojuku.ac.jp>
2024-11-28 03:43:18 +0900

    Update image_edit.php

--- kanrisya/image_edit.php
+++ kanrisya/image_edit.php
@@ -1,5 +1,5 @@
 <?php
-require('kanrisya_session.php'); 
+require('kanrisya_session.php');
 require_once '../DBconnect.php';
 
 $pdo = getDb();
@@ -17,14 +17,13 @@ $sql->execute([$car_id]);
 $images = $sql->fetchAll();
 ?>
 
-<form action="../kanrisya/image_update.php" method="POST" enctype="multipart/form-data">
+<form action="image_update.php" method="POST">
     <input type="hidden" name="car_id" value="<?= htmlspecialchars($car_id, ENT_QUOTES, 'UTF-8') ?>">
-    
+
     <h1 class="page-title">画像一覧</h1>
 
     <table class="image-table">
         <tr>
-            <!-- メイン画像 -->
             <td class="label-cell">メイン画像</td>
             <td class="image-cell">
                 <?php foreach ($images as $row): ?>
@@ -35,11 +34,9 @@ $images = $sql->fetchAll();
                         </label>
                     <?php endif; ?>
                 <?php endforeach; ?>
-                <input type="file" name="main_image" accept="image/*" required>
             </td>
         </tr>
         <tr>
-            <!-- サブ画像 -->
             <td class="label-cell">サブ画像</td>
             <td class="image-cell">
                 <?php foreach ($images as $row): ?>
@@ -50,13 +47,12 @@ $images = $sql->fetchAll();
                         </label>
                     <?php endif; ?>
                 <?php endforeach; ?>
-                <input type="file" name="other_images[]" accept="image/*" multiple>
             </td>
         </tr>
     </table>
 
     <div class="top-back-button">
-        <button type="submit" class="updateButton">選択した画像を編集する</button>
+        <button type="submit" class="updateButton">画像を選択してアップロード</button>
         <button type="button" class="back-button" onclick="location.href='car_list.php'">商品一覧へ戻る</button>
     </div>
 </form>
