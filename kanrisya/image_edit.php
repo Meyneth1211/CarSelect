6c1500d9fe2127939bf3d4f0f16024227e87d676
shimityoko9 <2401333@s.asojuku.ac.jp>
2024-11-28 04:38:22 +0900

    Update image_edit.php

--- kanrisya/image_edit.php
+++ kanrisya/image_edit.php
@@ -53,7 +53,6 @@ $images = $sql->fetchAll();
         
     <button type="submit" onclick="location.href='image_delete_confirm.php'">選択した画像を削除する</button>
     <div class="top-back-button">
-        <button type="submit" onclick="location.href='image_delete_confirm.php'">選択した画像を削除する</button>
         <button type="submit" class="updateButton">選択した画像を更新する</button>
         <button type="button" class="back-button" onclick="location.href='car_list.php'">商品一覧へ戻る</button>
     </div>
