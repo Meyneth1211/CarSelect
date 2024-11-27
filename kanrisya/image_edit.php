f90ab5f915ce60f39dc1abf107861dab8e763ef2
shimityoko9 <2401333@s.asojuku.ac.jp>
2024-11-27 10:43:08 +0900

    チェックボックスで選択した画像だけを更新

--- kanrisya/image_edit.php
+++ kanrisya/image_edit.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require('kanrisya_session.php'); 
 require_once '../DBconnect.php';
 
@@ -17,32 +17,26 @@ $sql->execute([$car_id]);
 $images = $sql->fetchAll();
 ?>
 
-<h1 class="page-title">画像一覧</h1>
+<form action="../kanrisya/image_update.php" method="POST">
+    <input type="hidden" name="car_id" value="<?= htmlspecialchars($car_id, ENT_QUOTES, 'UTF-8') ?>">
+    
+    <h1 class="page-title">画像一覧</h1>
 
-<table class="image-table">
-    <tr>
-        <!-- 項目名 -->
-        <td class="label-cell">メイン画像</td>
-        <td class="image-cell">
-            <?php foreach ($images as $row): ?>
-                <?php if ($row['is_primary'] == 1): ?>
-                    <img src="<?= htmlspecialchars($row['image'], ENT_QUOTES, 'UTF-8') ?>" alt="メイン画像" class="car-image">
-                <?php endif; ?>
-            <?php endforeach; ?>
-        </td>
-    </tr>
-    <tr>
-        <td class="label-cell">サブ画像</td>
-        <td class="image-cell">
-            <?php foreach ($images as $row): ?>
-                <?php if ($row['is_primary'] == 0): ?>
-                    <img src="<?= htmlspecialchars($row['image'], ENT_QUOTES, 'UTF-8') ?>" alt="サブ画像" class="car-image">
-                <?php endif; ?>
-            <?php endforeach; ?>
-        </td>
-    </tr>
-</table>
+    <table class="image-table">
+        <?php foreach ($images as $row): ?>
+            <tr>
+                <td class="label-cell"><?= $row['is_primary'] == 1 ? 'メイン画像' : 'サブ画像' ?></td>
+                <td class="image-cell">
+                    <img src="<?= htmlspecialchars($row['image'], ENT_QUOTES, 'UTF-8') ?>" alt="<?= $row['is_primary'] == 1 ? 'メイン画像' : 'サブ画像' ?>" class="car-image">
+                    <input type="checkbox" name="selected_images[]" value="<?= htmlspecialchars($row['image_id'], ENT_QUOTES, 'UTF-8') ?>">
+                    <label>編集対象</label>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
 
-<div class="top-back-button">
-    <button class="back-button" onclick="location.href='car_list.php'">商品一覧へ戻る</button>
-</div>
+    <div class="top-back-button">
+        <button type="submit" class="updateButton">選択した画像を編集する</button>
+        <button type="button" class="back-button" onclick="location.href='car_list.php'">商品一覧へ戻る</button>
+    </div>
+</form>
