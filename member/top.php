c6cbc96c39937fba1330962bc0beae213d714098
Meyneth1211 <2301389@s.asojuku.ac.jp>
2024-11-27 10:43:13 +0900

    値渡し用のJavaScriptを画像ボタンに追加

--- member/top.php
+++ member/top.php
@@ -51,44 +51,49 @@
 
   <form class="car-logo-back" action="search.php" method="get">
     <input type="hidden" name="s">
+    <input type="hidden" name="brands[]" id="sendbrand" value="">
     <div class="car-logo">
         <div class="car-logo-item">
-            <input type="image" name="brand" value="ベンツ" src="../img/Benz.png" alt="Benz">
+            <input type="image" name="brand" value="ベンツ" src="../img/Benz.png" alt="Benz" onclick="setBrandInfo('ベンツ')">
         </div>
         <div class="car-logo-item">
-            <input type="image" name="brand" value="日産" src="../img/Nissan.png" alt="Nissan">
+            <input type="image" name="brand" value="日産" src="../img/Nissan.png" alt="Nissan" onclick="setBrandInfo('日産')">
         </div>
         <div class="car-logo-item">
-            <input type="image" name="brand" value="マツダ" src="../img/Mazda.png" alt="Mazda">
+            <input type="image" name="brand" value="マツダ" src="../img/Mazda.png" alt="Mazda" onclick="setBrandInfo('マツダ')">
         </div>
         <div class="car-logo-item">
-            <input type="image" name="brand" value="トヨタ" src="../img/Toyota.png" alt="Toyota">
+            <input type="image" name="brand" value="トヨタ" src="../img/Toyota.png" alt="Toyota" onclick="setBrandInfo('トヨタ')">
         </div>
         <div class="car-logo-item">
-            <input type="image" name="brand" value="ホンダ" src="../img/Honda.png" alt="Honda">
+            <input type="image" name="brand" value="ホンダ" src="../img/Honda.png" alt="Honda" onclick="setBrandInfo('ホンダ')">
         </div>
     </div>
     <div class="car-logo2">
         <div class="car-logo-item">
-            <input type="image" name="brand" value="ポルシェ" src="../img/Porsche.png" alt="Porsche">
+            <input type="image" name="brand" value="ポルシェ" src="../img/Porsche.png" alt="Porsche" onclick="setBrandInfo('ポルシェ')">
         </div>
         <div class="car-logo-item">
-            <input type="image" name="brand" value="レクサス" src="../img/Lexus.png" alt="Lexus">
+            <input type="image" name="brand" value="レクサス" src="../img/Lexus.png" alt="Lexus" onclick="setBrandInfo('レクサス')">
         </div>
         <div class="car-logo-item">
-            <input type="image" name="brand" value="ランボルギーニ" src="../img/Lamborghini.png" alt="Lamborghini">
+            <input type="image" name="brand" value="ランボルギーニ" src="../img/Lamborghini.png" alt="Lamborghini" onclick="setBrandInfo('ランボルギーニ')">
         </div>
         <div class="car-logo-item">
-            <input type="image" name="brand" value="BMW" src="../img/BMW.png" alt="BMW">
+            <input type="image" name="brand" value="BMW" src="../img/BMW.png" alt="BMW" onclick="setBrandInfo('BMW')">
         </div>
         <div class="car-logo-item">
-            <input type="image" name="brand" value="フェラーリ" src="../img/Ferrari.png" alt="Ferrari">
+            <input type="image" name="brand" value="フェラーリ" src="../img/Ferrari.png" alt="Ferrari" onclick="setBrandInfo('フェラーリ')">
         </div>
     </div>
 </form>
 
 
-
+<script>
+  function setBrandInfo(value) {
+    document.getElementById('sendbrand').value = value;
+  }
+</script>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 <script src="../js/slide.js"></script>
