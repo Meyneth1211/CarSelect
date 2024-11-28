512faeb39776d5e9ccb68bf1950cd3ae2fcedfa7
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-28 10:05:48 +0900

    button追加

--- member/card.php
+++ member/card.php
@@ -23,10 +23,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <p><input class="card-button" type="submit" value="送信"></p>
             </div>
         </form>
-        <form action="car_detail.php" method="post">
-            <p><input class="card-button" type="submit" value="戻る"></p>
-            <input type="hidden" name="car_id" value="<?php echo htmlspecialchars($car_id, ENT_QUOTES, 'UTF-8'); ?>">
-        </form>
+        <div class="c-button"></div>
+            <button type="button" class="back-button" onclick="location.href='car_detail.php'">戻る</button>
+        <div class="top-back-button">
     </div>
 </div>
 </body>
