579c14e3aa4deb5daacfd303ca1142f4e448c153
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-28 16:35:10 +0900

    Update Purchase_confirmed.php

--- member/Purchase_confirmed.php
+++ member/Purchase_confirmed.php
@@ -13,9 +13,12 @@ if ($car_id) {
     $stmt = $pdo->prepare($sql);
     $stmt->execute([$car_id]);
 
+
+    echo '<div class="Purchase">';
+    echo '<div class="Purchase_confirmed">';
     // 更新された行数を確認
     if ($stmt->rowCount() > 0) {
-        $message = '在庫が更新されました。';
+        $message = '<h1>ご注文が完了しました</h1>';
     } else {
         $message = '在庫が不足しています。';
     }
@@ -23,10 +26,6 @@ if ($car_id) {
     $message = 'car_idが送信されていません。';
 }
 ?>
-
-<div class="Purchase">
-    <div class="Purchase_confirmed">
-        <h1>ご注文が完了しました</h1>
         <p><?php echo htmlspecialchars($message, ENT_QUOTES, 'UTF-8'); ?></p>
         <form class="Purchase-form" action="top.php" method="post">
             <input class="topback-button" type="submit" value="トップページに戻る">
