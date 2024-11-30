26e3cf30bc29ad6c6da481d276adddd66a7c4a5e
shimityoko9 <2401333@s.asojuku.ac.jp>
2024-12-01 06:55:56 +0900

    Update user_delete_confirm.php

--- kanrisya/user_delete_confirm.php
+++ kanrisya/user_delete_confirm.php
@@ -2,6 +2,7 @@
 require('kanrisya_session.php'); 
 require_once '../DBconnect.php';
 ?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -12,19 +13,20 @@ require_once '../DBconnect.php';
 <body>
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete_user_mail'])) {
-    $userId = $_POST['delete_user_mail'];
+    $userMail = $_POST['delete_user_mail'];
 } else {
     die('<p style="color: red;">不正なアクセスです。</p>');
 }
 ?>
 
+<h1>ユーザー削除確認</h1>
+<p>以下のユーザーを削除しますか？</p>
+<p>メールアドレス: <?= htmlspecialchars($userMail, ENT_QUOTES, 'UTF-8') ?></p>
+
 <form action="user_delete.php" method="post">
-    <div class="container">
-        <div class="message success">このユーザーを削除しますか？</div>
-        <input type="hidden" name="delete_id" value="<?= htmlspecialchars($userId, ENT_QUOTES, 'UTF-8') ?>">
-        <input type="submit" name="user-delete" value="削除する">
-        <button type="button" onclick="history.back();">戻る</button>
-    </div>
+    <input type="hidden" name="delete_user_mail" value="<?= htmlspecialchars($userMail, ENT_QUOTES, 'UTF-8') ?>">
+    <input type="submit" name="user-delete" value="削除する">
+    <button type="button" onclick="history.back();">戻る</button>
 </form>
 </body>
 </html>
