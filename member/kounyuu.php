a15881ff36c87c26807ca6acf7147e1385811069
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-30 17:54:09 +0900

    ボタンタグをインプットタグに変更

--- member/kounyuu.php
+++ member/kounyuu.php
@@ -53,7 +53,7 @@ if ($car_id) {
     <input class="kounyuu-button2" type="submit" value="購入確定">
 </form>
 <form class="kounyuu1-form" action="car_detail.php" method="post">
-    <input type="button" class="kounyuu-button1" onclick="location.href='car_detail.php?item=<?php echo htmlspecialchars($car_id, ENT_QUOTES, 'UTF-8'); ?>'">戻る</input>
+    <input type="button" class="kounyuu-button1" value="戻る" onclick="location.href='car_detail.php?item=<?php echo htmlspecialchars($car_id, ENT_QUOTES, 'UTF-8'); ?>'">
 </form>
 </div>
 </div>
