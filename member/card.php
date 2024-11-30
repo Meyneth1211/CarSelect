c15e3ac885cf7e749f82262cc23e1864e5dbc816
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-30 17:59:28 +0900

    Update card.php

--- member/card.php
+++ member/card.php
@@ -24,7 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             </div>
         </form>
         <form class="cardd-form" action="car_detail.php" method="get">
-            <button type="button" class="back-button" onclick="location.href='car_detail.php?item=<?php echo htmlspecialchars($car_id, ENT_QUOTES, 'UTF-8'); ?>'">戻る</button>
+            <input type="button" class="back-button" value="戻る" onclick="location.href='car_detail.php?item=<?php echo htmlspecialchars($car_id, ENT_QUOTES, 'UTF-8'); ?>'">
         </form>
 
     </div>
