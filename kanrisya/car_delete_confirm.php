13debe870fcc1c9601ea19d9292a96ac0374fc8c
shimityoko9 <2401333@s.asojuku.ac.jp>
2024-12-01 05:01:19 +0900

    Update car_delete_confirm.php

--- kanrisya/car_delete_confirm.php
+++ kanrisya/car_delete_confirm.php
@@ -8,15 +8,18 @@ $pdo = getDb();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>商品削除確認</title>
+    <style></style>
 </head>
 <body>
-<form action="car_delete.php" method="post">
-<div class="container">
-        <div class="message success">この商品を削除しますか？</div>
-        <input type="submit" name="car-delete" value="削除する">
-        <button type=""  onclick="history.back();">戻る</button> 
-</div>
-</form> 
+    <form action="car_delete.php" method="post">
+        <div class="container">
+            <div class="message success">
+                この商品を削除しますか？
+            </div>
+            <input type="submit" name="car-delete" value="削除する">
+            <button type="button" onclick="history.back();">戻る</button>
+        </div>
+    </form>
 </body>
 </html>
