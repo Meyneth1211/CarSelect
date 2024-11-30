c73871ccbad438e066dc71ca5145ad131e7401a8
RayOnoda <2301149@s.asojuku.ac.jp>
2024-11-30 17:48:52 +0900

    Update kounyuu.php

--- member/kounyuu.php
+++ member/kounyuu.php
@@ -47,12 +47,13 @@ if ($car_id) {
 }
 ?>
 
-<form class="kounyuu1-form" action="car_detail.php" method="post">
-    <input class="kounyuu-button1" type="submit" value="戻る">
-</form>
+
 <form class="kounyuu2-form" action="Purchase_confirmed.php" method="post">
     <input type="hidden" name="car_id" value="<?php echo htmlspecialchars($car_id, ENT_QUOTES, 'UTF-8'); ?>">
     <input class="kounyuu-button2" type="submit" value="購入確定">
+</form>
+<form class="kounyuu1-form" action="car_detail.php" method="post">
+    <button type="button" class="back-button" onclick="location.href='car_detail.php?item=<?php echo htmlspecialchars($car_id, ENT_QUOTES, 'UTF-8'); ?>'">戻る</button>
 </form>
 </div>
 </div>
