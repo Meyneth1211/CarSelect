3861600c78990fdadc2554529ee23b5d841fd9ee
shimityoko9 <2401333@s.asojuku.ac.jp>
2024-12-01 07:30:15 +0900

    Update user_delete_confirm.php

--- kanrisya/user_delete_confirm.php
+++ kanrisya/user_delete_confirm.php
@@ -20,13 +20,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete_user_mail'])) 
 ?>
 
 <h1>ユーザー削除確認</h1>
-<p>以下のユーザーを削除しますか？</p>
+<div class="container">
+<div class="message success">この商品を削除しますか？</div>
 <p>メールアドレス: <?= htmlspecialchars($userMail, ENT_QUOTES, 'UTF-8') ?></p>
-
 <form action="user_delete.php" method="post">
     <input type="hidden" name="delete_user_mail" value="<?= htmlspecialchars($userMail, ENT_QUOTES, 'UTF-8') ?>">
     <input type="submit" name="user-delete" value="削除する">
     <button type="button" onclick="history.back();">戻る</button>
+</div>
 </form>
 </body>
 </html>
