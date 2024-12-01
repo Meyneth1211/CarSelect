1adb2340452d94ac2e975193faa36ad5e0e05c40
shimityoko9 <2401333@s.asojuku.ac.jp>
2024-12-02 03:21:43 +0900

    Update image_delete_confirm.php

--- kanrisya/image_delete_confirm.php
+++ kanrisya/image_delete_confirm.php
@@ -14,7 +14,12 @@ if (isset($_POST['car_id']) && is_numeric($_POST['car_id'])) {
 if (isset($_POST['selected_images']) && is_array($_POST['selected_images'])) {
     $selected_image_ids = $_POST['selected_images'];
 } else {
-    die('削除する画像が選択されていません。');
+    echo '<div class="container">';
+    echo '<div class="message error">削除する画像が選択されていません</div>';
+    echo '<button type="button" class="save-button" onclick="history.back();">画像を選択する</button>';
+    echo '<button class="back-button" onclick="location.href=\'kanrisya_top.php\'">トップページへ戻る</button>';
+    echo '</div>';
+    exit;
 }
 
 // 選択された画像データを取得
