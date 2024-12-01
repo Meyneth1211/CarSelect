ede450ebdce88c8651e33dc64c55e7e8dea53a83
shimityoko9 <2401333@s.asojuku.ac.jp>
2024-12-02 00:30:14 +0900

    Update user_delete_confirm.php

--- kanrisya/user_delete_confirm.php
+++ kanrisya/user_delete_confirm.php
@@ -25,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete_user_mail'])) 
 <p>メールアドレス: <?= htmlspecialchars($userMail, ENT_QUOTES, 'UTF-8') ?></p>
 <form action="user_delete.php" method="post">
    <div class="button-group"> 
-    <input type="hidden" class="delete-button" name="delete_user_mail" value="<?= htmlspecialchars($userMail, ENT_QUOTES, 'UTF-8') ?>">
+    <input type="hidden" class="delete-confirm-button" name="delete_user_mail" value="<?= htmlspecialchars($userMail, ENT_QUOTES, 'UTF-8') ?>">
     <input type="submit" name="user-delete" value="削除する">
     <button type="button" class="back-button" onclick="history.back();">戻る</button>
    </div>
