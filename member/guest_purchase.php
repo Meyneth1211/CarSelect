1f084defdba6fcef280918602a004b060c75f5f7
RayOnoda <2301149@s.asojuku.ac.jp>
2024-12-03 10:49:22 +0900

    ゲスト購入画面修正

--- member/guest_purchase.php
+++ member/guest_purchase.php
@@ -1,15 +1,27 @@
 <?php require('../header/header.php'); ?>
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $car_id = $_POST['car_id'] ?? null;
+}
+?>
+
 <body>
     <div class="guestcreate">
         <div class="guestcreate-card">
-            <h2>ゲスト登録</h2>
+            <h2>ゲストで購入</h2>
             <form class="guestcreate-form" action="card.php" method="post" required>
                 <input class="guestname" name="name" type="text" placeholder="name" required>
                 <input class="guestmail" name="mail" type="email" placeholder="Email" required>
                 <input class="guestaddress" name="address" type="text" placeholder="address" required>
                 <div class="guesttop"></div>
+                <!-- car_id渡す -->
+                <input type="hidden" name="car_id" value="<?php echo htmlspecialchars($car_id, ENT_QUOTES, 'UTF-8'); ?>">
                 <input class="guestbutton" type="submit" value="登録">
             </form>
+            <div class="guestcreate-form">
+                <a class="guestbutton" href="">会員登録する</a>
+            </div>
         </div>
     </div>
 </body>
