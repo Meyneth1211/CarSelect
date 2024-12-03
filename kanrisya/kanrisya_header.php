d8c92efc2b26ad605da3f79c5da91c793910c361
RayOnoda <2301149@s.asojuku.ac.jp>
2024-12-03 14:26:37 +0900

    css追加

--- kanrisya/kanrisya_header.php
+++ kanrisya/kanrisya_header.php
@@ -18,15 +18,17 @@
     <title>Car Select kanrisya</title>
 </head>
 <body>
-    <div class="back">
+    <div class="header_back">
         <div class="hidden">
             <form action="kanrisya_logout.php" method="post">
                 <input type="image" src="../img/logout.png" width="50px" height="auto" alt="ログアウト">
             </form>
         </div>
 
-        <div class="header_logo">
-            <a class="logo_link" href="kanrisya_top">Car Select</a>
+        <div class="back">
+            <div class="logo_logo">
+                <a class="logo_link" href="kanrisya_top">Car Select</a>
+            </div>
         </div>
     </div>
 
