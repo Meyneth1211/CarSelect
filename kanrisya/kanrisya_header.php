900b9773effb010883847493796b35d65e933b2d
RayOnoda <2301149@s.asojuku.ac.jp>
2024-12-03 14:31:51 +0900

    Update kanrisya_header.php

--- kanrisya/kanrisya_header.php
+++ kanrisya/kanrisya_header.php
@@ -18,17 +18,15 @@
     <title>Car Select kanrisya</title>
 </head>
 <body>
-    <div class="header_back">
+    <div class="back">
         <div class="hidden">
             <form action="kanrisya_logout.php" method="post">
                 <input type="image" src="../img/logout.png" width="50px" height="auto" alt="ログアウト">
             </form>
         </div>
 
-        <div class="back">
-            <div class="logo_logo">
-                <a class="logo_link" href="kanrisya_top">Car Select</a>
-            </div>
+        <div class="header_logo">
+            <a class="logo_link" href="kanrisya_top">Car Select</a>
         </div>
     </div>
 
