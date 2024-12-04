a74de237bb18f7cac35601888c4137074cf0bbad
murakami722 <2301208@s.asojuku.ac.jp>
2024-12-04 09:57:01 +0900

    card

--- member/card.php
+++ member/card.php
@@ -12,7 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <form class="card-form" action="kounyuu.php" method="post">
             <h2>カード情報</h2>
             <div class="p-color">
-                <p class="mclass">カード番号<input class="card-no" type="text" name="cardno" maxlength="16" pattern="^[0-9]+$"></p>
+                <p class="mclass">カード番号<input class="card-no" type="text" name="cardno" maxlength="16" pattern="^[0-9]+$" title="半角数字のみ"></p>
                 <p class="mclass">セキュリティコード<input class="card-cord" type="password" name="cardpass" maxlength="3" size="5" required></p>
                 <p class="mclass">カード名義<input class="card-name" type="text" name="cardname" maxlength="50" required></p>
                 <p class="mclass">有効期限<input class="card-kigen" type="text" name="cardkigen1" maxlength="2" size="4" required>/<input class="card-kigen2" type="text" maxlength="2" size="4" required></p>
