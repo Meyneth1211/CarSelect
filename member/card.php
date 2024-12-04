7f53b653c8a582f6e8691c61e3a94a3f8a61c1e7
murakami722 <2301208@s.asojuku.ac.jp>
2024-12-04 09:43:00 +0900

    card number

--- member/card.php
+++ member/card.php
@@ -12,10 +12,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <form class="card-form" action="kounyuu.php" method="post">
             <h2>カード情報</h2>
             <div class="p-color">
-                <p class="mclass">カード番号<input class="card-no" type="text" name="cardno" maxlength="16" required></p>
-                <p class="mclass">セキュリティコード<input class="card-cord" type="password" name="cardpass" maxlength="3" size="5" required></p>
+                <p class="mclass">カード番号<input class="card-no" type="number" name="cardno" maxlength="16" required></p>
+                <p class="mclass">セキュリティコード<input class="card-cord" type="number" name="cardpass" maxlength="3" size="5" required></p>
                 <p class="mclass">カード名義<input class="card-name" type="text" name="cardname" maxlength="50" required></p>
-                <p class="mclass">有効期限<input class="card-kigen" type="text" name="cardkigen1" maxlength="2" size="4" required>/<input class="card-kigen2" type="text" maxlength="2" size="4" required></p>
+                <p class="mclass">有効期限<input class="card-kigen" type="number" name="cardkigen1" maxlength="2" size="4" required>/<input class="card-kigen2" type="text" maxlength="2" size="4" required></p>
             </div>
             <!-- car_id を 次のページに渡す -->
             <input type="hidden" name="car_id" value="<?php echo htmlspecialchars($car_id, ENT_QUOTES, 'UTF-8'); ?>">
