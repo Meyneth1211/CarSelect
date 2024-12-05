da348d253d2f98f12aba2acf8d07f2dc49b55ec7
taketyu <2301389@s.asojuku.ac.jp>
2024-12-05 01:15:58 +0000

    SQL周辺の処理を一通り作成

--- member/FavListHandler.php
+++ member/FavListHandler.php
@@ -3,7 +3,11 @@ require_once '../DBConnect.php';
 
 function getFavList($user){
     $pdo=getDB();
+    $sql='SELECT * FROM favorite WHERE user_id = ?';
+    $stmt=$pdo->prepare($sql);
+    $list=$pdo->execute([$user]);
     $pdo=null;
+    return $list;
 }
 
 function addFavItem($user, $car){
@@ -11,13 +15,17 @@ function addFavItem($user, $car){
     $sql='INSERT INTO favorite (user_id, car_id) VALUES (?,?)';
     $stmt=$pdo->prepare($sql);
     $result=$stmt->execute([$user,$car]);
-    return $result;
     $pdo=null;
+    return $result;
 }
 
 function delFavItem($user, $car){
     $pdo=getDB();
+    $sql='DELETE FROM favorite WHERE user_id = ? AND car_id = ? LIMIT 1';
+    $stmt=$pdo->prepare($sql);
+    $result=$stmt->execute([$user,$car]);
     $pdo=null;
+    return $result;
 }
 
 
