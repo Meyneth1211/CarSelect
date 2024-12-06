195575ed1dff8c9ffd46369f47a4779909a3c13b
Meyneth1211 <2301389@s.asojuku.ac.jp>
2024-12-06 10:16:29 +0900

    配列の添字を修正

--- member/favorites.php
+++ member/favorites.php
@@ -11,16 +11,18 @@ if (empty($_SESSION['id'])) {
     EOM;
     $favlist=getFavList($_SESSION['id']);
     //var_dump($favlist);
+    echo '<div class="car-list">';
     foreach ($favlist as $row) {
-        echo '<a href="https://aso2301389.hippy.jp/carselect/member/car_detail?item=' . $row['list']['car_id'] . '" class="car-item">'; // aタグを全体に適用
-        echo '<img src="' . $row['images']['image'] . '" alt="' . $row['list']['car_name'] . '">';
+        echo '<a href="https://aso2301389.hippy.jp/carselect/member/car_detail?item=' . $row[0]['car_id'] . '" class="car-item">'; // aタグを全体に適用
+        echo '<img src="' . $row[1]['image'] . '" alt="' . $row[0]['car_name'] . '">';
         echo '<div class="car-info">';
-        echo '<div class="search-car-date"><h3>' . $row['list']['car_name'] . '</h3>';
+        echo '<div class="search-car-date"><h3>' . $row[0]['car_name'] . '</h3>';
         echo '<div class="separator"></div>';
-        echo '<p>' . number_format($row['list']['price']) . '円</p>';
+        echo '<p>' . number_format($row[0]['price']) . '円</p>';
         echo '</div></div>';
         echo '</a>'; // aタグを閉じる
     }
+    echo '</div>';
 }
 
 
