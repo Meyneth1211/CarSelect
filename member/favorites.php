c5c197c9f02b3ce959f2ca61fbd0d4977b5ced64
Meyneth1211 <2301389@s.asojuku.ac.jp>
2024-12-06 10:00:45 +0900

    表示テスト

--- member/favorites.php
+++ member/favorites.php
@@ -10,7 +10,17 @@ if (empty($_SESSION['id'])) {
         </div>
     EOM;
     $favlist=getFavList($_SESSION['id']);
-    var_dump($favlist);
+    //var_dump($favlist);
+    foreach ($favlist as $row) {
+        echo '<a href="https://aso2301389.hippy.jp/carselect/member/car_detail?item=' . $row['list']['car_id'] . '" class="car-item">'; // aタグを全体に適用
+        echo '<img src="' . $row['images']['image'] . '" alt="' . $row['list']['car_name'] . '">';
+        echo '<div class="car-info">';
+        echo '<div class="search-car-date"><h3>' . $row['list']['car_name'] . '</h3>';
+        echo '<div class="separator"></div>';
+        echo '<p>' . number_format($row['list']['price']) . '円</p>';
+        echo '</div></div>';
+        echo '</a>'; // aタグを閉じる
+    }
 }
 
 
