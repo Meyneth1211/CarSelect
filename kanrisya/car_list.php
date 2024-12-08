5638ddde03e383da9dbf5b771f2145ec9fed1e61
RayOnoda <2301149@s.asojuku.ac.jp>
2024-12-08 20:16:28 +0900

    一覧画面3桁区切りカンマ追加

--- kanrisya/car_list.php
+++ kanrisya/car_list.php
@@ -84,7 +84,7 @@ $cars = $sql->fetchAll(PDO::FETCH_ASSOC);
       <tr>
         <td><?= htmlspecialchars($row['brand'], ENT_QUOTES, 'UTF-8') ?></td>
         <td><?= htmlspecialchars($row['car_name'], ENT_QUOTES, 'UTF-8') ?></td>
-        <td><?= htmlspecialchars($row['price'],  ENT_QUOTES, 'UTF-8') ?></td>
+        <td><?= number_format(htmlspecialchars($row['price'],  ENT_QUOTES, 'UTF-8')) ?></td>
         <td><?= htmlspecialchars($row['body_type'], ENT_QUOTES, 'UTF-8') ?></td>
         <td><?= htmlspecialchars($row['color'], ENT_QUOTES, 'UTF-8') ?></td>
         <td>
