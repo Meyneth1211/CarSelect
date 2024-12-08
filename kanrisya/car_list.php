e0c96842b14d8774c9332e9eee7d18168df27205
shimityoko9 <2401333@s.asojuku.ac.jp>
2024-12-08 14:02:51 +0900

    Update car_list.php

--- kanrisya/car_list.php
+++ kanrisya/car_list.php
@@ -30,7 +30,7 @@ $cars = $sql->fetchAll(PDO::FETCH_ASSOC);
       border: 1px solid #ddd;
       border-radius: 5px;
       transition: background-color 0.3s, color 0.3s;
-      gap: 20px;
+      margin: 0 20px;
      }
 
      .brand-buttons{
