4733cc0a46f53d81604923a0b71611a7819084ca
Meyneth1211 <2301389@s.asojuku.ac.jp>
2024-12-10 12:34:19 +0900

    お気に入り追加/削除完了時に元いたページにリダイレクトするよう設定

--- member/FavListEditer.php
+++ member/FavListEditer.php
@@ -2,13 +2,15 @@
 session_start();
 require 'FavListHandler.php';
 if ($_SERVER['REQUEST_METHOD']==='POST') {
-    if (empty($_SESSION['id']) || empty($_POST['car_id']) || empty($_POST['action'])) {
+    if (empty($_SESSION['id']) || empty($_POST['car_id']) || empty($_POST['action']) || empty($_POST['url'])) {
         header('Location: top.php');
     } else {
         if ($_POST['action']=='add') {
             addFavItem($_SESSION['id'],$_POST['car_id']);
+            echo '<script>window.location.href = "'. $_POST['url'].'";</script>';
         }elseif ($_POST['action']=='del') {
             delFavItem($_SESSION['id'],$_POST['car_id']);
+            echo '<script>window.location.href = "'. $_POST['url'].'";</script>';
         }
     }
 } else {
--- member/search.php
+++ member/search.php
@@ -303,6 +303,7 @@ if (isset($_GET['s'])) {
         echo '<form action="FavListEditer.php" method="post">';
           echo '<input type="hidden" name="car_id" value="'.$row['car_id'].'">';
           echo '<input type="hidden" name="action" value="del">';
+          echo '<input type="hidden" name="url" value="'. $_SERVER['REQUEST_URI'].'">';
           echo '<button type="submit">';
             echo '♥<!-- <img src="icon.png" alt="Submit" style="width: 24px; height: 24px;"> -->';
           echo '</button>';
@@ -311,6 +312,7 @@ if (isset($_GET['s'])) {
         echo '<form action="FavListEditer.php" method="post">';
         echo '<input type="hidden" name="car_id" value="'.$row['car_id'].'">';
         echo '<input type="hidden" name="action" value="add">';
+        echo '<input type="hidden" name="url" value="'. $_SERVER['REQUEST_URI'].'">';
           echo '<button type="submit">';
             echo '♡<!--<img src="icon.png" alt="Submit" style="width: 24px; height: 24px;"> -->';
           echo '</button>';
