083b3cbb8178416ef56eec1cc04186e51eb49cd7
shimityoko9 <2401333@s.asojuku.ac.jp>
2024-12-11 10:31:17 +0900

    Update top.php

--- member/top.php
+++ member/top.php
@@ -52,7 +52,7 @@
   }
 </script>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vegas/2.4.4/vegas.min.js"></script>
 <script src="../js/slide.js"></script>
 </body>
 </html>
