0179a0a017e8ecaa5a29e7e1f39838c8103469a2
Meyneth1211 <2301389@s.asojuku.ac.jp>
2024-12-11 19:44:20 +0900

    ログアウト時はトップページへ遷移するように

--- member/logout.php
+++ member/logout.php
@@ -1,5 +1,5 @@
 <?php
-    header('Location: https://aso2301389.hippy.jp/carselect/member/login.php');//他の画面に遷移したいとき。先頭に書く。
+    header('Location: https://aso2301389.hippy.jp/carselect/member/top.php');//他の画面に遷移したいとき。先頭に書く。
     session_start();
 
     $_SESSION = array();//空の配列を渡すセッション中身消す
