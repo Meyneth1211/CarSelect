fd3011128cc5f3add6115738919740dce8a49f48
shimityoko9 <2401333@s.asojuku.ac.jp>
2024-12-11 10:31:13 +0900

    Update slide.js

--- js/slide.js
+++ js/slide.js
@@ -1,21 +1,36 @@
-$('.slider').slick({
-	fade:true,//切り替えをフェードで行う。初期値はfalse。
-	autoplay: true,//自動的に動き出すか。初期値はfalse。
-	autoplaySpeed: 3000,//次のスライドに切り替わる待ち時間
-	speed:1000,//スライドの動きのスピード。初期値は300。
-	infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-	slidesToShow: 1,//スライドを画面に3枚見せる
-	slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
-	arrows: true,//左右の矢印あり
-	prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-	nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-	dots: true,//下部ドットナビゲーションの表示
-	pauseOnFocus: false,//フォーカスで一時停止を無効
-	pauseOnHover: false,//マウスホバーで一時停止を無効
-	pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
-});
+//画像と動画の設定
+var windowwidth = window.innerWidth || document.documentElement.clientWidth || 0;
+		if (windowwidth > 768){
+			var responsiveImage = [//PC用の動画と画像
+				{ src: './img/img_01.jpg',//動画が再生されなかった場合の代替画像
+				 video:{
+					 src: [//mp4で動画が再生されない時のことを考えて複数の形式の動画を設定
+						 './video/movie.mp4',
+						 './video/movie.webm',
+						 './video/movie.ogv'
+					 ],
+					 loop: false,//動画を繰り返さない
+					 mute: true,//動画の音を鳴らさない
+				 }
+				},
+				{src: './img/img_02.jpg'},
+				{src: './img/img_03.jpg'}
+			];
+		} else {
+			var responsiveImage = [//タブレットサイズ（768px）以下用の画像
+				{ src: './img/img_sp_01.jpg' },
+				{ src: './img/img_sp_02.jpg' },
+				{ src: './img/img_sp_03.jpg' }
+			];
+}
 
-//スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
-$('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
-$('.slider').slick('slickPlay');
-});
+//Vegas全体の設定
+$('#slider').vegas({
+		overlay: true,//画像の上に網線やドットのオーバーレイパターン画像を指定。
+		transition: 'fade2',//切り替わりのアニメーション。http://vegas.jaysalvat.com/documentation/transitions/参照。fade、fade2、slideLeft、slideLeft2、slideRight、slideRight2、slideUp、slideUp2、slideDown、slideDown2、zoomIn、zoomIn2、zoomOut、zoomOut2、swirlLeft、swirlLeft2、swirlRight、swirlRight2、burnburn2、blurblur2、flash、flash2が設定可能。
+		transitionDuration: 2000,//切り替わりのアニメーション時間をミリ秒単位で設定
+		delay: 5000,//スライド間の遅延をミリ秒単位で。
+		animationDuration: 20000,//スライドアニメーション時間をミリ秒単位で設定
+		animation: 'random',//スライドアニメーションの種類。http://vegas.jaysalvat.com/documentation/transitions/参照。kenburns、kenburnsUp、kenburnsDown、kenburnsRight、kenburnsLeft、kenburnsUpLeft、kenburnsUpRight、kenburnsDownLeft、kenburnsDownRight、randomが設定可能。
+		slides: responsiveImage,//画像と動画の設定を読む
+	});
